18430a77d026b7c90432d6fd1a46336e3bfd1688
OGabrielPereira <OGabrielPereira@pm.me>
2023-02-18 03:57:35 -0300

    First Commit

---
+++ lib/pages/start_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
