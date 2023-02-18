81811c42ba40c1b2c5398f3a0d6533047fe85ce5
OGabrielPereira <OGabrielPereira@pm.me>
2023-02-18 04:27:54 -0300

    StartPage created

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Think.do',
+      debugShowCheckedModeBanner: false,
       home: StartPage(),
     );
   }
--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -5,10 +5,49 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text('Hello World'),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 127, 145, 173),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/images/startpage.png',
+              width: 350,
+              height: 350
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+              'Welcome to the Think.do', 
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+ 
+          MaterialButton(
+            onPressed: () {}, 
+            color: const Color.fromARGB(255, 201, 237, 212),
+            child: const Text(
+              'Get Started',
+              style: TextStyle(
+                color: Color.fromARGB(255, 127, 145, 173),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Text(
+              'Already have account? Sign in',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        ],
       ),
     );
   }
