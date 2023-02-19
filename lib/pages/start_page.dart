7eec7c2c4a095b69643f079428d998f05ec2ed43
OGabrielPereira <OGabrielPereira@pm.me>
2023-02-19 00:43:35 -0300

    StartPage 90%

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -18,16 +18,33 @@ class StartPage extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
-          const Text(
-              'Welcome to the Think.do', 
+          RichText(
+            text: const TextSpan(
+              text: '    welcome to the \n',
               style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+                color: Colors.white, 
+                fontSize: 28,
+                fontFamily: 'BebasNeue', 
               ),
+              children: <TextSpan> [
+                TextSpan(
+                  text: 'think.do',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 66, 84, 121), 
+                    fontSize: 65
+                  ),
+                ),
+              ],
             ),
- 
+          ),
+
+          const SizedBox(height: 10),
           MaterialButton(
+            minWidth: 300,
+            height: 60,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
             onPressed: () {}, 
             color: const Color.fromARGB(255, 201, 237, 212),
             child: const Text(
@@ -39,14 +56,27 @@ class StartPage extends StatelessWidget {
               ),
             ),
           ),
-          const Text(
-              'Already have account? Sign in',
+          const SizedBox(height: 10),
+          RichText(
+            text: const TextSpan(
+              text: 'Already have account? ',
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.white, 
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
               ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: 'Sign In',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 66, 84, 121), 
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
+          ),
         ],
       ),
     );
